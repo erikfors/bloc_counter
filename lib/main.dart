0a7b15d1832b44523db348dcabf36a7a37f12a53
erikfors <efors003@fiu.edu>
2022-02-01 18:21:42 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_counter/counter_observer.dart';
+
+import 'app.dart';
+
+void main() {
+  BlocOverrides.runZoned(
+      () => runApp(
+            const CounterApp(),
+          ),
+      blocObserver: CounterObserver());
+}
